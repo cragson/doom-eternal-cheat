07e343ba42a1651b5ce5bf496fb037ddd3b7f58c
Paul <cragson@outlook.de>
2021-08-21 22:45:30 +0200

    0011

---
+++ cheat/ft_god_mode.cpp
@@ -0,0 +1,31 @@
+#include "ft_god_mode.hpp"
+#include "SDK/globals.hpp"
+#include "SDK/Offsets/offsets.hpp"
+
+void ft_god_mode::tick()
+{
+}
+
+void ft_god_mode::on_enable()
+{
+	const auto game_image_base = Globals::g_pProcess->get_image_base(L"DOOMEternalx64vk.exe");
+
+	if (!game_image_base)
+		return;
+	
+	Globals::g_pProcess->write< char >( game_image_base + Offsets::god_mode_patch, 0xEB );
+}
+
+void ft_god_mode::on_disable()
+{
+	const auto game_image_base = Globals::g_pProcess->get_image_base(L"DOOMEternalx64vk.exe");
+
+	if (!game_image_base)
+		return;
+	
+	Globals::g_pProcess->write< char >( game_image_base + Offsets::god_mode_patch, 0x75 );
+}
+
+void ft_god_mode::on_first_activation()
+{
+}
---
+++ cheat/ft_god_mode.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Osmium/Cheat/Feature/feature.hpp"
+
+class ft_god_mode : public feature
+{
+public:
+
+	void tick() override;
+
+	void on_enable() override;
+
+	void on_disable() override;
+
+	void on_first_activation() override;
+};
